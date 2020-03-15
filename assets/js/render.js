51e7973000c05c54254d102b380bc0c52ea983a8
nox7 <17110935+nox7@users.noreply.github.com>
2020-03-15 17:03:23 -0500

    Add CodeSupport 3D logo

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,28 +1,25 @@
 import {OrbitControls} from "./three/OrbitControls.js";
+import RoundedBox from "./_rounded-box.js";
 
 (() => {
 	const scene = new THREE.Scene;
 	const container = document.querySelector("#render-window");
 	const camera = new THREE.PerspectiveCamera(55, container.clientWidth/container.clientHeight, 0.1, 30000);
-	const ambientLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 7 );
+	const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 	const renderer = new THREE.WebGLRenderer();
 
-	camera.position.set(-5, 5, 7); // Must be non 0,0,0 for controls to work
+	camera.position.set(0,10,-100); // Must be non 0,0,0 for controls to work
+
 	renderer.setSize( container.clientWidth, container.clientHeight );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.gammaFactor = 2.2;
 	renderer.physicallyCorrectLights = true;
 	renderer.setSize(container.clientWidth, container.clientHeight);
 
-	//scene.background = new THREE.Color( 0x200e2b );
-	//scene.fog = new THREE.Fog(0x200e2b, 2000, 2500);
-	//this.ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
-	//this.directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-	//this.directionalLight.position.set(10,10,10);
-	//this.scene.add(this.ambientLight, this.directionalLight);
-
-
 	const controls = new OrbitControls(camera, renderer.domElement);
+	camera.lookAt(0,10,50);
+	controls.target.set(0,10,50);
+	controls.maxDistance = 1000;
 
 	// Load textures and add them to an array of mesh materials
 	const textureLoader = new THREE.TextureLoader();
@@ -58,6 +55,10 @@ import {OrbitControls} from "./three/OrbitControls.js";
 	const skybox = new THREE.Mesh(skyboxGeometry, materialArray);
 
 	const dotGeometry = new THREE.Geometry();
+	const roundedBox = new RoundedBox(200, 65, 5, 2, 1);
+	const roundedBoxMaterial = new THREE.MeshLambertMaterial({
+		color:new THREE.Color("rgb(20,87,179)")
+	});
 	const dotMaterial = new THREE.PointsMaterial({size:1, sizeAttenuation:false});
 	const dots = new THREE.Points(dotGeometry, dotMaterial);
 
@@ -66,11 +67,46 @@ import {OrbitControls} from "./three/OrbitControls.js";
 		dotGeometry.vertices.push(new THREE.Vector3(getRandomNumber(-1000, 1000),  getRandomNumber(-700, 700), getRandomNumber(-1000, 1000)));
 	}
 
+	const codeSupportBox = new THREE.Mesh(roundedBox.geometry, roundedBoxMaterial);
+	codeSupportBox.position.set(0,10,50);
+
 	// Add items to scene
 	scene.add(dots);
 	scene.add(ambientLight);
 	scene.add(skybox);
+	scene.add(codeSupportBox);
 
+	// Add lights
+	const directionalLight = new THREE.DirectionalLight(new THREE.Color("rgb(50,50,50)"), 5);
+	directionalLight.position.set(0,100,-50);
+	directionalLight.target = codeSupportBox;
+	scene.add(directionalLight);
+
+	// Add the text
+	const fontLoader = new THREE.FontLoader();
+	fontLoader.load("../assets/fonts/helvetiker_bold.typeface.json", (font) => {
+		const textGeometry = new THREE.TextGeometry("CodeSupport", {
+			font:font,
+			size:"20",
+			height:"1",
+			curveSegments:4,
+			bevelThickness:1,
+			bevelSize:1,
+			bevelEnabled:true
+		});
+
+		const textMesh = new THREE.Mesh(textGeometry, [
+			new THREE.MeshPhongMaterial( { color: new THREE.Color("rgb(100,100,100)")} ), // front
+			new THREE.MeshPhongMaterial( { color: new THREE.Color("rgb(100,100,100)")} ) // side
+		]);
+
+		textMesh.rotation.set(0,Math.PI,0);
+		textMesh.position.set(87, 0, 51)
+
+		scene.add(textMesh);
+	});
+
+	// Add the canvas
 	container.appendChild(renderer.domElement);
 
 	function getRandomNumber(min, max){
